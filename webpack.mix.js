bb6ed07a7c114bab85f900631654f8866f9cbde2
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2021-01-13 15:50:15 +0200

    Rename dist dir to src

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 const mix = require('laravel-mix');
 
 mix
-    .js('dist/js/libraries.js', 'public/js') // comment out
-    .js('dist/js/projects.js', 'public/js')
-    .sass('dist/scss/libraries.scss', 'public/css') // comment out
-    .sass('dist/scss/projects.scss', 'public/css');
+    .js('src/js/libraries.js', 'public/js') // comment out
+    .js('src/js/projects.js', 'public/js')
+    .sass('src/scss/libraries.scss', 'public/css') // comment out
+    .sass('src/scss/projects.scss', 'public/css');
